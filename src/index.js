dace3318061d2e3e2804f94dbc0bfff778919a1c
Sulaymon333 <tjsulaymon@gmail.com>
2019-04-13 18:41:53 +0300

    comment react app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import Message from './Message';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:40PM"
+          content="Great blog post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 5:00PM"
+          content="Great blog mango!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <CommentDetail
+        author="Mango"
+        timeAgo="Today at 2:00AM"
+        content="Great blog Papaya!"
+        avatar={faker.image.avatar()}
+      />
+
+      <CommentDetail
+        author="Apple"
+        timeAgo="Today at 1:00AM"
+        content="Great blog Apple!"
+        avatar={faker.image.avatar()}
+      />
+
+      <Message
+        header="Attention!"
+        text="We just updated our privacy policy here to better service our customers."
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
