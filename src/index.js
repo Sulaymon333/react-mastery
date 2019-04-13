a8f5d9592e623b264182d3ba3659b420d92729fa
Sulaymon333 <tjsulaymon@gmail.com>
2019-04-13 20:33:07 +0300

    moved window.getlocation funcrtion to constructor to prevent multiple fetch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      console.log(position);
-    },
-    err => {
-      console.log(err);
-    }
-  );
-  return <div>Hello class components</div>;
-};
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     position => {
+//       console.log(position);
+//     },
+//     err => {
+//       console.log(err);
+//     }
+//   );
+//   return <div>Hello class components</div>;
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+
+    // get latitude function is moved to the constructor to prevent double fetch when render() is called multiple times
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  render() {
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
