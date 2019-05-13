351afc7c12a66bd0c825ec09733350de60bd3f5c
Sulaymon333 <tjsulaymon@gmail.com>
2019-05-13 16:51:53 +0300

    react-router-dom components - Switch, BrowserRouter, Link, NavLink, Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 
-function App() {
+const Home = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Home</h1>
     </div>
   );
+};
+
+const About = props => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+const Contact = props => {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+const HTMLPage = props => {
+  return (
+    <div>
+      <h3>Let us discuss HTML</h3>
+    </div>
+  );
+};
+
+const CSSPage = props => {
+  return (
+    <div>
+      <h3>Let us discuss CSS</h3>
+    </div>
+  );
+};
+
+const JSPage = props => {
+  return (
+    <div>
+      <h3>Let us discuss JS</h3>
+    </div>
+  );
+};
+const REACTPage = props => {
+  return (
+    <div>
+      <h3>Let us discuss REACT</h3>
+    </div>
+  );
+};
+
+const Topics = props => {
+  console.log(props);
+  const path = props.match.path;
+  console.log(path);
+  return (
+    <div>
+      <h2>Topics</h2>
+      <hr />
+      <ul>
+        <li>
+          <Link to={`${path}/html`}>HTML</Link>
+        </li>
+
+        <li>
+          <Link to={`${path}/css`}>CSS</Link>
+        </li>
+        <li>
+          <Link to={`${path}/js`}>JAVASCRIPT</Link>
+        </li>
+        <li>
+          <Link to={`${path}/react`}>REACT</Link>
+        </li>
+      </ul>
+      <Route path={`${path}/html`} component={HTMLPage} />
+      <Route path={`${path}/css`} component={CSSPage} />
+      <Route path={`${path}/js`} component={JSPage} />
+      <Route path={`${path}/react`} component={REACTPage} />
+    </div>
+  );
+};
+
+const User = props => {
+  const username = props.match.params.username;
+  return (
+    <div>
+      <h4>Welcome {username}</h4>
+    </div>
+  );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <ul>
+          <li>
+            {/* NavLink has more functionalities like active and so on */}
+            <NavLink to="/">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+          </li>
+          <li>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+          <li>
+            <Link to="/topics">TOPICS</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about" render={About} />
+          <Route path="/contact" render={Contact} />
+          <Route path="/topics" render={Topics} />
+          <Route path="/" Component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
