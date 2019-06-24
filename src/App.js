6a13acdc868f490e99289e0b8c3a0c53acddd5d8
Sulaymon333 <tjsulaymon@gmail.com>
2019-06-24 15:14:49 +0300

    initial project setup with basic todos rendered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const Todo = ({ todo, index }) => {
+    return <div className="todo">{todo.text}</div>;
+};
+
+const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState('');
+
+    const handleChange = e => setValue(e.target.value);
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" className="input" value={value} onChange={handleChange} />
+        </form>
+    );
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([
+        {
+            text: 'I wanna learn react hooks',
+            isComplete: false
+        },
+        {
+            text: 'I wanna learn react router',
+            isComplete: true
+        },
+        {
+            text: 'I wanna learn something cool',
+            isComplete: false
+        },
+        {
+            text: 'I wanna build and amazing voting app',
+            isComplete: false
+        }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, text];
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="app">
+            {todos.map((todo, index) => (
+                <Todo key={index} index={index} todo={todo} />
+            ))}
+            <TodoForm addTodo={addTodo} />
+        </div>
+    );
 }
 
 export default App;
