5a5a258505da89078238921758581db93e344c06
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-16 18:51:33 +0300

    TodoItem component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>my name</h1>
+const App = () => (
+    <>
+        <h1 style={{ margin: ' 3rem 0', textAlign: 'center' }}>Awesome Todo-App</h1>
+        <div className="todo-list">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </div>
-    );
-}
+    </>
+);
 
 export default App;
---
+++ src/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoItem = () => (
+    <div className="todo-item">
+        <input type="checkbox" />
+        <p>Some todo to execute</p>
+    </div>
+);
+
+export default TodoItem;
