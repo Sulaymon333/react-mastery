9c70b1bca9dfd83ed7ac4087d5f0dea603996e62
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-17 14:35:02 +0300

    implemented completed toggle functionality

--- src/App.js
+++ src/App.js
@@ -8,9 +8,28 @@ class App extends Component {
         this.state = {
             todos: todosData
         };
+
+        this.handleChange = this.handleChange.bind(this);
     }
+    handleChange(id) {
+        // console.log('Changed', id);
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            });
+            return {
+                todos: updatedTodos
+            };
+        });
+    }
+
     render() {
-        const todoItems = this.state.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
+        const todoItems = this.state.todos.map(todo => (
+            <TodoItem key={todo.id} todo={todo} handleChange={this.handleChange} />
+        ));
         return (
             <>
                 <h1 style={{ margin: ' 3rem 0', textAlign: 'center' }}>Awesome Todo-App</h1>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const TodoItem = ({ todo }) => (
+const TodoItem = ({ todo, handleChange }) => (
     <div className="todo-item">
-        <input type="checkbox" checked={todo.completed} />
+        <input type="checkbox" checked={todo.completed} onChange={() => handleChange(todo.id)} />
         <p>{todo.text}</p>
     </div>
 );
