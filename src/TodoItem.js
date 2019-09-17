22464e09d0c700b9993344c0ef73fe5f5cdc47d5
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-17 16:49:02 +0300

    completes item style implemented

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const TodoItem = ({ todo, handleChange }) => (
-    <div className="todo-item">
-        <input type="checkbox" checked={todo.completed} onChange={() => handleChange(todo.id)} />
-        <p>{todo.text}</p>
-    </div>
-);
+const TodoItem = ({ todo, handleChange }) => {
+    const completedStyle = {
+        color: '#3aaaa3',
+        fontStyle: 'italic',
+        textDecoration: 'line-through'
+    };
+
+    return (
+        <div className="todo-item">
+            <input type="checkbox" checked={todo.completed} onChange={() => handleChange(todo.id)} />
+            <p style={todo.completed ? completedStyle : null}>{todo.text}</p>
+        </div>
+    );
+};
 
 export default TodoItem;
