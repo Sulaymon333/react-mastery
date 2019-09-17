b01f32693d7ace571088a4fa03d306014e0a52ba
Sulaymon333 <tjsulaymon@gmail.com>
2019-09-17 13:28:16 +0300

    App component converted to a state component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import todosData from './todosData';
 
-const App = () => {
-    const todoItems = todosData.map(todo => <TodoItem key={todo.id} todo={todo} />);
-    return (
-        <>
-            <h1 style={{ margin: ' 3rem 0', textAlign: 'center' }}>Awesome Todo-App</h1>
-            <div className="todo-list">{todoItems}</div>
-        </>
-    );
-};
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: todosData
+        };
+    }
+    render() {
+        const todoItems = this.state.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
+        return (
+            <>
+                <h1 style={{ margin: ' 3rem 0', textAlign: 'center' }}>Awesome Todo-App</h1>
+                <div className="todo-list">{todoItems}</div>
+            </>
+        );
+    }
+}
 
 export default App;
